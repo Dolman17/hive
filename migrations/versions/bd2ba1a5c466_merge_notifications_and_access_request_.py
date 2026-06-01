86a586d079e410fd913cb7c9767ca16263791e60
Dolman17 <seandolman17@gmail.com>
2026-06-01 21:18:49 +0100

    Merge Alembic heads for access requests

---
+++ migrations/versions/bd2ba1a5c466_merge_notifications_and_access_request_.py
@@ -0,0 +1,24 @@
+"""Merge notifications and access request heads
+
+Revision ID: bd2ba1a5c466
+Revises: 20260529_notifications, 20260601_consultant_access_requests
+Create Date: 2026-06-01 21:18:23.290624
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd2ba1a5c466'
+down_revision = ('20260529_notifications', '20260601_consultant_access_requests')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
